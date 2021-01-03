42135285db3fa80db2e4eec45f9ce48d4b0d242f
tonewheelcreations <68931996+tonewheelcreations@users.noreply.github.com>
2021-01-02 22:35:17 -0500

    added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,20 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
